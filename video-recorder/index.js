8d2beea0aaebf269928a199626fc8081983ae88e
WarriorsSami <sbarbutdica@gmail.com>
2024-08-23 13:23:09 +0300

    fix(video-recorder): set resume marker after end to avoid re-recording of video in case of failure in final chunk

--- video-recorder/index.js
+++ video-recorder/index.js
@@ -184,6 +184,7 @@ async function recordVideo(video) {
     }
 
     await sleep(40 * 1000);
+    await setVideoResumeMarker(video.slug, resumeMarker);
     // await deleteVideoResumeMarker(video.slug);
     console.log("Video recorded: " + video.name);
     // await publishVideoRecordedEvent(video);
